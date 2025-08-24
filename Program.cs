947f09ac388326629e744406e7dcaabeae3e798f
ahmedgndy <ahmed.mamdouh.elgndy@gmail.com>
2025-08-24 15:43:18 +0300

    feat(config): implement WeatherSettings class with configuration binding

--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//register the weather settings
+builder.Services.Configure<WeatherSettings>(builder.Configuration.GetSection("WeatherSettings"));  
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/get-weather", (IOptions<WeatherSettings> options) =>
+{
+    var weatherSettings = options.Value;
+
+    var weatherData = new Dictionary<string, string>
+    {
+        { "City", weatherSettings.DefaultCity },
+        { "TemperatureUnit", (30 + " " + weatherSettings.TemperatureUnit).ToString() }
+    };
+    return weatherData;
+
+});
+
 
 app.Run();
+
